dedc40fb97265388161016fba5da1a89db72e620
Ruth-Braga <54600822+Ruth-Braga@users.noreply.github.com>
2020-12-14 13:59:04 -0300

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -80,8 +80,8 @@ $table_prefix = 'wp_';
 define( 'WP_DEBUG', false );
 
 /* That's all, stop editing! Happy publishing. */
-define ( 'WP_HOME' , 'https://www.akamine.daaula.com.br' );
-define ( 'WP_SITEURL', 'https://www.akamine.daaula.com.br' );
+define ( 'WP_HOME' , 'https://akamine.daaula.com.br' );
+define ( 'WP_SITEURL', 'https://akamine.daaula.com.br' );
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/' );
